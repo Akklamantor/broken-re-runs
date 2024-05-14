ca2ac9162df1f054efbf7ef64ba0ce1ba4fc13e5
Akklamantor <vladimir.iakovlev@qameta.io>
2024-05-14 16:51:02 +0300

    k

---
+++ README.md
@@ -0,0 +1,1 @@
+## sample allure-pytest project
---
+++ test/set_id_and_retry_test.py
@@ -0,0 +1,34 @@
+import pytest
+import allure
+
+
+@pytest.mark.parametrize("test_id", [140988, 140989])
+@allure.severity(allure.severity_level.NORMAL)
+def test_refactored(test_id):
+    allure.dynamic.id(test_id)
+
+
+@allure.id(140987)
+@allure.severity(allure.severity_level.CRITICAL)
+def test_renamed():
+    pass
+
+@allure.severity(allure.severity_level.MINOR)
+def test_new_test_with_severity():
+    pass
+
+@allure.severity(allure.severity_level.TRIVIAL)
+def test_one_more_new_test_with_severity():
+    pass
+
+@allure.label("layer", "UI Tests")
+def test_ui_test():
+    pass
+
+@allure.manual
+def test_manual():
+    pass
+
+@pytest.mark.parametrize(["a", "b"], [(1, "a"), (2, "b")])
+def test_parametrized(a, b):
+    pass
